8039a137f3b7672659f55171b6d8648e21a93202
tuanddd <tuanddd@users.noreply.github.com>
2024-02-16 07:42:45 +0000

    deploy: f2de94e38f7d7858bc254c69fb96552c97d65ca6

---
+++ js/share.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+  document.addEventListener("mousedown", (e) => {
+    const tooltip = document.querySelector("#tooltip");
+    console.log(e.target);
+    if (tooltip === e.target || tooltip.contains(e.target)) return;
+    tooltip.classList.remove("pop--visible");
+  });
+  document.addEventListener("mouseup", () => {
+    const tooltip = document.querySelector("#tooltip");
+    const selection = window.getSelection();
+    const text = selection.toString();
+    if (!text) return;
+    tooltip.classList.add("pop--visible");
+
+    tooltip.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      text + "\n" + window.location.href
+    )}`;
+    Popper.createPopper(selection.getRangeAt(0), tooltip, {
+      placement: "top",
+      modifiers: [
+        {
+          name: "offset",
+          options: {
+            offset: [0, 10],
+          },
+        },
+      ],
+    });
+  });
+
+  console.log("share script init ok");
+};
